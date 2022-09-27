adb165e4c502427bccf6d724438ed2fc9312178c
James Kahoro <jameskahoro07@gamail.com>
2022-09-27 20:40:48 +0300

    about img

--- src/components/aboutimg.js
+++ src/components/aboutimg.js
@@ -1,40 +1,15 @@
-import { graphql, useStaticQuery } from "gatsby";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import React from "react";
-
-const query = graphql`
-	{
-		file(relativePath: { eq: "dev.png" }) {
-			publicURL
-			relativePath
-			childImageSharp {
-				gatsbyImageData(
-					placeholder: BLURRED
-					pngOptions: { compressionSpeed: 10, quality: 10 }
-				)
-			}
-		}
-	}
-`;
+import Dev from "../images/dev.png";
 
 function Aboutimg() {
-	const data = useStaticQuery(query);
-	const image = getImage(data.file);
-	console.log(image);
 	return (
 		<div className="imgstyle-two">
 			<div className="image-wrapper">
-				<div className="img-item">
-					<GatsbyImage image={image} className="image-1" alt="James Kahoro" />
-				</div>
-				<div className="img-item">
-					<GatsbyImage image={image} className="image-2" alt="James Kahoro" />
-				</div>
-				<div className="img-item">
-					<GatsbyImage image={image} className="image-3" alt="James Kahoro" />
-				</div>
-				<div className="img-item">
-					<GatsbyImage image={image} className="image-4 " alt="James Kahoro" />
+				<div className="image-wrapper">
+					<img className="image-1" src={Dev} alt="" />
+					<img className="image-2" src={Dev} alt="" />
+					<img className="image-3" src={Dev} alt="" />
+					<img className="image-4" src={Dev} alt="" />
 				</div>
 			</div>
 		</div>
